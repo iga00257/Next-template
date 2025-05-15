f61c2f1dccb7cdd78fdd9a3395857dce64f72c30
Derrick_yt Liu <derrick_ytliu@tutorabc.com>
2025-05-15 16:50:36 +0800

    feat: update i18next config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-
 const LOCALES_ENUM = {
   EN: 'en',
   ZH_TW: 'zh-TW',
@@ -7,6 +6,7 @@ const LOCALES_ENUM = {
 }
 
 const LOCALES = Object.values(LOCALES_ENUM)
+const FALLBACK_LOCALE = LOCALES_ENUM.EN
 
 /** @type {import('next-i18next').UserConfig} */
 const i18nConfig = {
@@ -15,6 +15,8 @@ const i18nConfig = {
     locales: [...LOCALES, 'default'],
   },
   defaultNS: 'common',
+  fallbackLng: [FALLBACK_LOCALE],
   localePath: path.resolve('./public/locales'),
+  reloadOnPrerender: process.env.DEPLOY_ENV === 'development',
 }
 module.exports = i18nConfig
